d63ec8fa80d24e786454786cc25a3d6f1bd72a38
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-21 12:48:39 +0100

    some updates

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Card = ({ title, description, imageSrc,srcLink }) => {
   return (
@@ -14,16 +15,17 @@ const Card = ({ title, description, imageSrc,srcLink }) => {
       maxW="sm"
       w="100%"
     >
-      <Image src={imageSrc} alt={title} borderRadius="md" />
+      <Image src={imageSrc} alt={title} borderRadius="md" style={{width:"100%",
+            height:"auto" }} />
       <VStack spacing={2} alignItems="start">
         <Heading color="black" as="h2" size="md">
           {title}
         </Heading>
-        <Text color="black">{description}</Text>
+        <Text color="black" >{description}</Text>
       </VStack>
       <HStack justifyContent="flex-end">
-        <Text style={{cursor:"pointer"}} color="blue.500"><a href={srcLink}>{console.log(srcLink)}Learn More</a> </Text> 
-        <FontAwesomeIcon color="red" icon={faArrowRight} size="1x" />
+        <Text style={{cursor:"pointer"}} color="blue.500"><a href={srcLink}>{console.log(srcLink)}<FontAwesomeIcon icon={faGithub} bounce size="2xl"/> </a> </Text> 
+        
       </HStack>
     </VStack>
   );
